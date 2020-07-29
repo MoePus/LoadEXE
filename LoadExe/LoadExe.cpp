dba67e5f8efa3220bca5a2f8fff5da0a0470e96b
MoePus <547007249@qq.com>
2020-07-29 15:51:35 +0800

    first commit

---
+++ LoadExe/LoadExe.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#define NOMINMAX
+#include "windows.h"
+#include "InjectInit.h"
+
+int main()
+{
+    DWORD hWnd;
+    std::cout << "Input hwnd (oct): ";
+    std::cin >> hWnd;
+    SelfInject((HWND)hWnd);
+    return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+    {
+        MessageBoxA(0, "123", "456", 0);
+        return 0; // return 1 to keep exe in injected process.
+    }
+    
+    return 1;
+}
